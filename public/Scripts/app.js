295f65271b6c717a9a53fe19ab1b3f57d486a147
rnavarr9 <rnavarr9@my.centennialcollege.ca>
2021-09-28 16:48:38 -0400

    Adds button and mission to Home page

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -4,3 +4,47 @@
     }
     window.addEventListener("load", Start)
 })()
+
+/* ---- Start particles.js  ---- */
+particlesJS("particles-js", {
+    particles: {
+      number: {
+        value: 250,
+        density: {
+          enable: true,
+          value_area: 800
+        }
+      },
+      color: {
+        value: "#969494"
+      },
+      opacity: {
+        value: 0.5,
+        random: false,
+        anim: {
+          enable: false,
+          speed: 1,
+          opacity_min: 0.1,
+          sync: false
+        }
+      },
+      size: {
+        value: 3,
+        random: true,
+        anim: {
+          enable: false,
+          speed: 40,
+          size_min: 0.1,
+          sync: false
+        }
+      },
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: "#969494",
+        opacity: 0.4,
+        width: 1
+      }
+    }
+  });
+  /* ---- End particles.js  ---- */
