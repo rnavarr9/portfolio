23d06be1b5874efeb07c543dc3da5a0592f7724f
rnavarr9 <rnavarr9@my.centennialcollege.ca>
2021-09-29 01:16:03 -0400

    Implements about me pages with own partials

--- routes/index.js
+++ routes/index.js
@@ -1,36 +1,35 @@
-var express = require('express');
+var express = require("express");
+var socialNetworksData = require("../public/javascripts/data");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home', pageName: "Home Page" });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Home", pageName: "Home Page" });
 });
 
 /* GET home page. */
-router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Home', pageName: "Home Page" });
+router.get("/home", function (req, res, next) {
+  res.render("index", { title: "Home", pageName: "Home Page" });
 });
 
-
 /* GET About page. */
-router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About', pageName: "About Page" });
+router.get("/about", function (req, res, next) {
+  res.render("about", { title: "About", pageName: "About Page", socialNetworksData });
 });
 
 /* GET Product page. */
-router.get('/projects', function(req, res, next) {
-  res.render('index', { title: 'Projects', pageName: "Projects Page" });
+router.get("/projects", function (req, res, next) {
+  res.render("index", { title: "Projects", pageName: "Projects Page" });
 });
 
 /* GET Services page. */
-router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services', pageName: "Services page" });
+router.get("/services", function (req, res, next) {
+  res.render("index", { title: "Services", pageName: "Services page" });
 });
 
 /* GET Contact us page. */
-router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact', pageName: "Contact page" });
+router.get("/contact", function (req, res, next) {
+  res.render("index", { title: "Contact", pageName: "Contact page" });
 });
-
 
 module.exports = router;
